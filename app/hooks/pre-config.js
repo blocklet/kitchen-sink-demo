7a6e10afa2b7fe868c0fbde7e6c9b6bba7480d22
wangshijun <wangshijun2010@gmail.com>
2023-08-26 16:30:54 +0800

    chore: add more test case

--- app/hooks/pre-config.js
+++ app/hooks/pre-config.js
@@ -5,6 +5,12 @@ console.log('#################################');
 console.log('### This is pre config hook ###');
 console.log('#################################');
 
+console.log('process.env.SECURE_ENV1', process.env.SECURE_ENV1);
+console.log('process.env.SECURE_ENV2', process.env.SECURE_ENV2);
+if (process.env.SECURE_ENV1 === '1') {
+  throw new Error('error from SECURE_ENV1');
+}
+
 if (!['yes', 'no'].includes(process.env.ONLY_YES_OR_NO)) {
   console.error('ONLY_YES_OR_NO can only be yes or no');
   process.exit(1);
---
+++ migration/1.js
@@ -0,0 +1,4 @@
+const range = require('lodash/range');
+
+// eslint-disable-next-line no-console
+console.log('range from blocklet-dapp-complex migration', range(1, 10));
